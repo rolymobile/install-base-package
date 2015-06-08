f189a1a29ce5ecb2eee0688dfe0d72be2b9d843a
ming7824 <admin@rolymobile.com>
2015-06-08 13:21:14 +0800

    Create install_package.sh

---
+++ install_package.sh
@@ -0,0 +1,13 @@
+apt-get update
+apt-get install joe
+apt-get install gcc -y
+apt-get install make
+apt-get install g++ -y
+apt-get install unzip
+apt-get install lrzsz
+apt-get install libpam-dev
+apt-get install libpcre3 libpcre3-dev
+apt-get install libssl-dev openssl -y
+apt-get install libxslt-dev -y
+apt-get install libgd2-xpm libgd2-xpm-dev -y
+apt-get install libgeoip-dev -y
